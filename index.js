fa83cca091422ed8607fe39e380020d5dfcb8cc1
BalRockY <71256208+BalRockY@users.noreply.github.com>
2025-01-31 00:59:55 +0100

    click function to open mobile menu + footer media query

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // locate elements
   const hamburgerIcon = document.getElementById("hamburger-icon");
   const mobileMenu = document.querySelector(".mobile-menu");
-  console.log("Menu element found:", mobileMenu); // Check if it logs the UL element
+
+  let isClickable = true; // buttonclick enabled
 
   if (hamburgerIcon && mobileMenu) {
+    // if elements are located, add event listener in the form of a click function
     hamburgerIcon.addEventListener("click", function () {
-      mobileMenu.classList.toggle("open");
-      console.log("Menu open:", mobileMenu); // Check if it logs the UL element
-      console.log("run toggle function");
+      if (isClickable) {
+        // toggle menu class name to add or remove the classname "open", toggling the mobile-menu
+        mobileMenu.classList.toggle("open");
+        isClickable = false; // buttonclick temporarily disabled, since fast clicking would prevent toggling in some cases.
+        setTimeout(function () {
+          isClickable = true; // reenable buttonclick after a delay
+        }, 100); // delay-time. Set as needed
+      }
     });
   }
 });
