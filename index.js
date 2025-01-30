68c75d9a26b940f89e71d022b26d8615a2be7017
BalRockY <71256208+BalRockY@users.noreply.github.com>
2025-01-31 00:19:17 +0100

    dropdown mobile menu

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function toggleMobileMenu(menu) {
-  menu.classList.toggle("open");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerIcon = document.getElementById("hamburger-icon");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  console.log("Menu element found:", mobileMenu); // Check if it logs the UL element
+
+  if (hamburgerIcon && mobileMenu) {
+    hamburgerIcon.addEventListener("click", function () {
+      mobileMenu.classList.toggle("open");
+      console.log("Menu open:", mobileMenu); // Check if it logs the UL element
+      console.log("run toggle function");
+    });
+  }
+});
